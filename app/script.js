1fe076a49ec7855354369527d7a397edb43e3b34
heathercrew <heather1@live.unc.edu>
2018-12-02 23:37:46 -0500

    work on filtering

--- app/script.js
+++ app/script.js
@@ -1,31 +1,119 @@
 // Globals
+var origin = "";
 var dest = "";
 var date;
+var data;
 
 // JQuery Logic
 $(document).ready(function() {
     
-    $('#destInput').keypress( function(event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode == '13'){
-            dest = $(this).val();
-            $(".in").empty();
-            $(".in").html("<p>Which day would you like to travel?</p><input id='dateInput' type='date' name='dest'>");
-            handleDate();
+    //Login
+    login();
+    
+    //Begin taking input.
+    $("#dateInput").datepicker({
+        format: "yyyy-mm-dd",
+        onSelect: function() { 
+            date = $(this).datepicker('getDate'); 
+            $(".in").html("<p>Where are you flying from? Please input an airport code.</p><input id='originInput' type='text'>");
+            var rightMonth = date.getMonth()+1;
+            var d = date.getFullYear() + "-" + rightMonth + "-" + date.getDate();
+            
+            $.ajax({
+                xhrFields: {withCredentials: true},
+                type: "GET",
+                url: "http://comp426.cs.unc.edu:3001/instances?filter[date]=" + d
+            }).done(function(data) {
+                data = data.filter(entry => entry.date == d); //making extra fucking sure
+                var flightIDs = data.map(entry => entry.flight_id);
+                var idInfo = {};
+                for (var i = 0; i < flightIDs.length; i++) {
+                    (function(i) { // Mr Closure
+                        $.ajax({
+                            xhrFields: {withCredentials: true},
+                            type: "GET",
+                            async: false,
+                            url: "http://comp426.cs.unc.edu:3001/flights?filter[id]=" + flightIDs[i]
+                        }).done(function(data1) {
+                            data1 = data1.filter(x => x.id === flightIDs[i]); //making extra fucking sure
+                            var dept_id = data1[0].departure_id;
+                            var arr_id = data1[0].arrival_id;
+                            var dept = "";
+                            var arr = "";
+                            //grab dept airport
+                            (function(dept_id, dept) {
+                                $.ajax({
+                                    xhrFields: {withCredentials: true},
+                                    type: "GET",
+                                    async: false,
+                                    url: "http://comp426.cs.unc.edu:3001/airports?filter[id]=" + dept_id
+                                }).done(function(d) {
+                                    d = d.filter(x => x.id === dept_id);
+                                    dept = d[0].code;
+                                    console.log("Departure from " + dept);
+                                });
+                            })(dept_id, dept);
+                            
+                            (function(arr_id, arr) {
+                                $.ajax({
+                                    xhrFields: {withCredentials: true},
+                                    type: "GET",
+                                    async: false,
+                                    url: "http://comp426.cs.unc.edu:3001/airports?filter[id]=" + arr_id
+                                }).done(function(d) {
+                                    d = d.filter(x => x.id === arr_id);
+                                    arr = d[0].code;
+                                    console.log("Arrival to " + dept);
+                                });
+                            })(arr_id, arr);
+                            
+                        });
+                    })(i);
+                }
+            });
+            handleOrigin();
         }
     });
 
     
 });
 
-function handleDate() {
-    $("#dateInput").keypress(function(event) {
+function handleOrigin() {
+    $('#originInput').keypress( function(event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode == '13'){
-            date = $(this).val();
+            origin = $(this).val();
             $(".in").empty();
-            //handle the ajax call here
-            $(".in").html("<p>Awesome! Here are your options. If you want to buy a ticket, click the 'Buy' button next to the entry.</p>");
+            $(".in").html("<p>Where are you flying to?</p><input id='destInput' type='text'>");
+            handleDest();
         }
+    });
+}
+
+function handleDest() {
+    $("#destInput").keypress(function(event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13'){
+            dest = $(this).val();
+            $(".in").empty();
+            $(".in").html("<p>Displaying ajax response...</p>");
+            handleDate();
+        }
+    });
+}
+
+function login() {
+    $.ajax({
+        url: 'http://comp426.cs.unc.edu:3001/sessions',
+        type: 'POST',
+        data: {
+            user: {
+                username: 'tabathav',
+                password: '730010609',
+            },
+        },
+        xhrFields: {
+            withCredentials: true
+        },
     });
 }
